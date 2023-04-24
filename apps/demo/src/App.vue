d8b8e17f76947ec8c2213a01dd29e0bc17e107be
liuyongzhi <50040262+overdev-l@users.noreply.github.com>
2023-04-24 11:31:40 +0800

    feat: add vue demo

--- apps/demo/src/App.vue
+++ apps/demo/src/App.vue
@@ -1,6 +1,13 @@
 <template>
-  <div>demo</div>
+  <n-config-provider :theme="darkTheme">
+    <Container />
+  </n-config-provider>
 </template>
 
 <style scoped>
 </style>
+
+<script lang="ts" setup>
+  import { darkTheme } from 'naive-ui'
+  import Container from './player/Layout.vue'
+</script>
---
+++ apps/demo/src/player/Layout.vue
@@ -0,0 +1,7 @@
+<template>
+    <div>
+        123123
+    </div>
+</template>
+
+<script lang="ts" setup></script>
