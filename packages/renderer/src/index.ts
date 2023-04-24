e9f5c8a997f909b4e587aafdae0a10663a4c1d1a
liuyongzhi <50040262+overdev-l@users.noreply.github.com>
2023-04-24 15:12:00 +0800

    chore: test packages dev

---
+++ packages/renderer/src/index.ts
@@ -0,0 +1,12 @@
+const a = "123213"
+console.log(a)
+console.log(a + "123")
+interface ITest {
+    data: string[]
+}
+const test: ITest = {
+    data: ["123", "123",],
+}
+console.log(test)
+console.log(test)
+console.log(test)
