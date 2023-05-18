8c72e3e658ebc770682877d7567441a10e214ed2
liuyongzhi <50040262+overdev-l@users.noreply.github.com>
2023-05-18 22:18:09 +0800

    test: time controller

--- packages/time-controller/__test__/timeController.test.ts
+++ packages/time-controller/__test__/timeController.test.ts
@@ -1,7 +1,32 @@
-import { describe, expect, it, } from "vitest"
+import { test, expect, vi, } from "vitest"
+import { TimeController, } from "../src/index"
 
-describe("test", () => {
-    it("测试", () => {
-        expect(1 + 1).toBe(2)
+test("TimeController basic functionality", async () => {
+    const mockOnTimeUpdate = vi.fn()
+    const mockRenderPlay = vi.fn()
+    const mockRenderPause = vi.fn()
+    const mockNextFiber = vi.fn()
+
+    const timeController = new TimeController({
+        duration: 10000,
+        onTimeUpdate: mockOnTimeUpdate,
+        play: mockRenderPlay,
+        pause: mockRenderPause,
+        nextFiber: mockNextFiber,
     })
+
+    expect(mockOnTimeUpdate).toBeCalledTimes(1)
+
+    timeController.setCurrentTime(5000)
+    expect(timeController.currentDuration).toBe(5000)
+    expect(timeController.playerStatus).toBe(2)
+
+    timeController.play()
+    expect(mockRenderPlay).toBeCalledTimes(1)
+
+    timeController.pause()
+    expect(mockRenderPause).toBeCalledTimes(2)
+
+    await timeController.next()
+    expect(mockNextFiber).toBeCalledTimes(1)
 })
