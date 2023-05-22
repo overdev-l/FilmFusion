154754682f83ed976389dd88e92039fd549b6594
liuyongzhi <50040262+overdev-l@users.noreply.github.com>
2023-05-22 16:31:03 +0800

    feat: ffmpeg

--- packages/generate/src/index.ts
+++ packages/generate/src/index.ts
@@ -4,22 +4,25 @@ import ParseConfig from "../../parser/src/types"
 const generate = async (log: boolean, fiber: ParseConfig.SceneFiber | null) => {
     if (!fiber) return
     const inputs = []
-    let duration = 0
-    let filter = ""
-    while (fiber) {
-        const videPath = fiber.sceneData.movie.url
-        const startTime = fiber.sceneData.movie.startTime
-        const endTime = fiber.sceneData.movie.endTime
-        duration += endTime - startTime
-        filter += `[${inputs.length - 1}:v] [${inputs.length - 1}:a] `
-        inputs.push(`-i ${videPath} -ss ${startTime} -to ${endTime} -async 1`)
-        fiber = fiber.nextScene
-    }
-    const output = `-filter_complex "${filter}concat=n=${inputs.length}:v=1:a=1[out]" -map "[out]" -t ${duration} -c:v libx264 -c:a aac -strict -2 output.mp4`
+    const duration = 0
+    const filter = ""
+    const videPath = fiber.sceneData.movie.url
+    const startTime = fiber.sceneData.movie.startTime
+    const endTime = fiber.sceneData.movie.endTime
+    // while (fiber) {
+    //     const videPath = fiber.sceneData.movie.url
+    //     const startTime = fiber.sceneData.movie.startTime
+    //     const endTime = fiber.sceneData.movie.endTime
+    //     duration += endTime - startTime
+    //     filter += `[${inputs.length - 1}:v] [${inputs.length - 1}:a] `
+    //     inputs.push(`-i ${videPath} -ss ${startTime} -to ${endTime} -async 1`)
+    //     fiber = fiber.nextScene
+    // }
+    // const output = `-filter_complex "${filter}concat=n=${inputs.length}:v=1:a=1[out]" -map "[out]" -t ${duration} -c:v libx264 -c:a aac -strict -2 output.mp4`
 
     const ffmpeg = createFFmpeg({ log, })
     await ffmpeg.load()
-    await ffmpeg.run(...inputs, output)
+    await ffmpeg.run(`-i ${videPath} -ss ${startTime} -to ${endTime} -async 1`, "output.mp4")
 }
 
 export {
